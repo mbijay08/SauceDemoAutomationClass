fc8e9e271932c71c4e03814294a4fd43f2a10b79
utsav-joshi123 <warriorutsav@gmail.com>
2023-08-03 19:50:19 -0400

    added list to which takes expected items

--- src/test/java/CheckoutPge.java
+++ src/test/java/CheckoutPge.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CheckoutPge extends BasePage {
-    public CheckoutPge(WebDriver driver){
+    public CheckoutPge(WebDriver driver) {
         super(driver);
     }
 
@@ -17,41 +17,54 @@ public class CheckoutPge extends BasePage {
     @FindBy(id = "add-to-cart-sauce-labs-backpack")
     private WebElement backpack;
     @FindBy(id = "add-to-cart-sauce-labs-bolt-t-shirt")
-    private  WebElement tshirt;
+    private WebElement tshirt;
     @FindBy(id = "shopping_cart_container")
     private WebElement cart;
     @FindBy(id = "checkout")
     private WebElement checkoutBtn;
-    @FindBy(id="first-name")
+    @FindBy(id = "first-name")
     private WebElement firstName;
-    @FindBy(id="last-name")
+    @FindBy(id = "last-name")
     private WebElement lastName;
-    @FindBy(id="postal-code")
+    @FindBy(id = "postal-code")
     private WebElement zipCode;
     @FindBy(id = "continue")
     private WebElement continueBtn;
 
-    public boolean isOnCheckoutPage(){
+    public boolean isOnCheckoutPage() {
         return driver.getCurrentUrl().contains("checkout-step-one.html");
     }
-    public void pickItems(){
+
+    public void pickItems() {
         backpack.click();
         tshirt.click();
         cart.click();
     }
-    public void checkout(){
+
+    public void checkout() {
         checkoutBtn.click();
     }
-    public void userInfo(String firstname, String lastname, String zip){
+
+    public void userInfo(String firstname, String lastname, String zip) {
         firstName.sendKeys(firstname);
         lastName.sendKeys(lastname);
         zipCode.sendKeys(zip);
         continueBtn.click();
     }
 
-    public List<String> getItemNames(){
+    public List<String> getItemNames() {
         return itemNames.stream().map(WebElement::getText).collect(Collectors.toList());
     }
+
+    public List<String> getExpectedItem(String string1, String string2) {
+        List<String> expectedItem = new ArrayList<>();
+        expectedItem.add(string1);
+        expectedItem.add(string2);
+        return expectedItem;
+    }
+}
+
+
 
     /*public List<String> itemNames(){
         List<String> names = new ArrayList<>();
@@ -60,4 +73,3 @@ public class CheckoutPge extends BasePage {
         }
         return names;
     }*/
-}
